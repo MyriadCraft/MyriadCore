60a4b7db31df803909889d950060e933e3d80083
Oliver Wright <meiamsome@hotmail.com>
2012-01-21 19:49:53 +0000

    First commit

---
+++ MyriadCore/src/me/meiamsome/myriadbase/MyriadPlugin.java
@@ -0,0 +1,11 @@
+package me.meiamsome.myriadbase;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public abstract class MyriadPlugin extends JavaPlugin {
+	@Override
+	public void onEnable() {
+		// TODO Auto-generated method stub
+		
+	}
+}
---
+++ MyriadCore/src/me/meiamsome/myriadcore/MyriadCore.java
@@ -0,0 +1,19 @@
+package me.meiamsome.myriadcore;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class MyriadCore extends JavaPlugin{
+
+	@Override
+	public void onDisable() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onEnable() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
